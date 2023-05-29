1561671248aa41d1c2535f764e70efb1536ee494
Anna-Ashton <ann.karanja@student.moringaschool.com>
2023-05-29 13:52:36 +0300

    created folders and files to set file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Footer, Blog, Feature, Possibility, Whatgpt3, Header} from './containers';
+import { Cta, Brand, Navbar } from './components'
+
 
 function App() {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <Whatgpt3 />
+      <Feature />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   )
 }
